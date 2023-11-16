dfe93172b6068724e8a11587480121cab64b94de
miottofelipee <123502192+miottofelipee@users.noreply.github.com>
2023-11-16 17:05:22 -0300

    aaaaaa

--- src/controllers/animais.controller.js
+++ src/controllers/animais.controller.js
@@ -3,13 +3,16 @@ import { Animal } from "../models/animais/Animais.js"
 import { AnimaisRepository } from "../models/animais/AnimaisRepository.js"
 
 const listAnimais = new AnimaisRepository();
+const Url = (url) => {
+    return url.match(/\.(jpeg|jpg|gif|png)$/) == null;
+}
 
 export const getAnimais = (req, res) => {
     const animais = listAnimais.getAnimais();
     if (animais.length) {
-        return res.status(200).json(animais);
+        return res.status(200).send({animais, quantidade: animais.length});
     }
-    return res.status(200).json({ message: "Não há animais cadastrados" });
+    return res.status(404).json({ message: "Não há animais cadastrados" });
 };
 
 export const getAnimal = (req, res) => {
@@ -25,8 +28,35 @@ export const createAnimal = (req, res) => {
     const { nome, idade, tipo, cor, status, img } = req.body;
     const animal = new Animal(nome, idade, tipo, cor, status, img);
 
-    listAnimais.addAnimais(animal);
+    if( !nome || !idade || !tipo || !cor || !img){
+        return res.status(400).send("Dados insuficientes");
+    }
 
+    if(nome.length > 50 || nome.length <3){
+        return res.status(400).send('O tamanho do nome deve ser entre 3 e 50');
+    }
+
+    if(tipo.length > 30){
+        return res.status(400).send('Tipo de animal muito grande!');
+    }
+
+    if(cor.length > 20){
+        return res.status(400).send('Cor muito grande!');
+    }
+
+    if(idade < 0 || !(Number.isInteger(idade))){
+        return res.status(400).send({message:"Idade inválida"});
+    }
+
+    if(!(Url(img))){
+        return res.status(400).send({message:"Formato da imagem inválida"});
+    }
+
+    if(typeof(status) !== "boolean"){
+        return res.status(400).send({mensage: "Status de vacinção invalido"})
+    }
+
+    listAnimais.addAnimais(animal);
     return res.status(201).send(animal);
 };
 
